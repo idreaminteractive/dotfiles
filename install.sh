382fb63e4f2712119643d211fc46295979eaf509
Dave Wiper <dwiper@idreaminteractive.com>
2023-08-22 10:46:37 +0000

    chore: almost there

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get tmux
-sudo apt-get -y install tmuxß
+sudo apt-get -y install tmux
 
 # don't run zsh automatically
 export RUNZSH=no
@@ -9,14 +9,30 @@ export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
 
 # download and install starship prompt
-curl -sS https://starship.rs/install.sh 
+curl -sS https://starship.rs/install.sh --output starship.sh
+
+# make it runnable
+chmod u+x starship.sh
 
 # install starship without a prompt
-./install.sh -y
+./starship.sh -y
 
 # use our thing
-cp .zshrc ~
+cp /home/gitpod/.dotfiles/.zshrc ~
 
+# install nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+# install lvim
+curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh -o lunar.sh
+chmod u+x lunar.sh
+LV_BRANCH='release-1.3/neovim-0.9' bash ./lunar.sh -y
+
+cd $GITPOD_REPO_ROOT
 # init our tmux 
-tmux
+tmux 
 
