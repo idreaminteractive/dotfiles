2b8e87bdea4d0b688ce45eab5778626a19c63faf
Dave Wiper <dwiper@idreaminteractive.com>
2023-09-01 15:26:57 +0000

    chore: some more config and better tmux bindings

---
+++ .bashrc
@@ -0,0 +1,7 @@
+eval "$(starship init bash)"
+
+eval "$(zoxide init bash)"
+
+alias ll="exa -l -g --icons"
+alias llt="exa -1 --icons --tree"
+alias ll="exa -l -g --icons --all"
--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ sudo apt-get -y install tmux
 
 sudo apt install exa
 # don't run zsh automatically
-export RUNZSH=no
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
+# export RUNZSH=no
+# # install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
 
 # download and install starship prompt
 curl -sS https://starship.rs/install.sh --output starship.sh
@@ -19,18 +19,19 @@ chmod u+x starship.sh
 ./starship.sh -y
 
 # use our thing
-cat /home/gitpod/.dotfiles/.zshrc | tee -a ~/.zshrc >/dev/null
+# cat /home/gitpod/.dotfiles/.zshrc | tee -a ~/.zshrc >/dev/null
+cat /home/gitpod/.dotfiles/.bashrc | tee -a ~/.bashrc >/dev/null
 # ripgrep
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
 
 # install nvim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-./squashfs-root/AppRun --version
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+# chmod u+x nvim.appimage
+# ./nvim.appimage --appimage-extract
+# ./squashfs-root/AppRun --version
+# sudo mv squashfs-root /
+# sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 
 # we're doing a custom nvim lesgo.
@@ -42,10 +43,16 @@ cd $GITPOD_REPO_ROOT
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
 
+
+# load our starship stuff
+cp /home/gitpod/.dotfiles/starship.toml ~/.config/starship.toml
+starship preset bracketed-segments -o ~/.config/starship.toml
+
+
 # run tmux
 # copy our bindings.
 cp -r /home/gitpod/.dotfiles/.tmux.conf ~/.tmux.conf
 
-tmux 
+tmux -u
 
 
