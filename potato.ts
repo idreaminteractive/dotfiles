56f42e3970670ffee94a3a27860f7f085b40057e
Dave Wiper <dwiper@idreaminteractive.com>
2023-09-05 10:00:10 +0000

    readded in nvim

--- potato.ts
+++ potato.ts
@@ -1,2 +1,9 @@
 console.log("Hey now, you're an allstar")
 console.log("Things look like they are working properly");
+
+
+(async() => {
+
+	console.log("We are in an async function here");
+
+} )()
