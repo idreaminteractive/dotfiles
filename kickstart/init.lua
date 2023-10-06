49307dfae4b235b8e9713a08d276752ddb6ce084
Dave <dwiper@idreaminteractive.com>
2023-10-06 18:14:03 -0400

    Update init.lua

--- kickstart/init.lua
+++ kickstart/init.lua
@@ -595,7 +595,7 @@ vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") e
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>t", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-i>", function() ui.nav_file(1) end)
