22f8b2ccc3779be428911020130ec74f3f7f8ead
Dave <dwiper@idreaminteractive.com>
2023-10-07 15:14:15 -0400

    Update init.lua

--- kickstart/lua/custom/plugins/init.lua
+++ kickstart/lua/custom/plugins/init.lua
@@ -5,14 +5,15 @@
 return {
 	-- { "tjdevries/templ.nvim",    lazy = false },
 	{ "nvim-tree/nvim-tree.lua", lazy = false },
-	{ 'ThePrimeagen/harpoon' }
-	, {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-}
+	{ 'ThePrimeagen/harpoon' },
+	{'ThePrimeagen/vim-be-good'}, 
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	}
 }
