26562261849b9287f44541de5d51e8b1c4b9ac91
Dave <dwiper@idreaminteractive.com>
2025-01-31 07:33:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y exa
+sudo apt install -y gpg
+
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 # download and install starship prompt
 curl -sS https://starship.rs/install.sh --output starship.sh
