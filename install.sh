fec3cc408af7256cbef82d07b4c5125e0ae7b009
Dave Wiper <dwiper@idreaminteractive.com>
2025-07-16 10:06:29 +0000

    chore: zellij config

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ chmod u+x starship.sh
 cat ~/.dotfiles/bash_aliases | tee  ~/.bash_aliases >/dev/null
 cat ~/.dotfiles/bash_profile | tee ~/.bash_profile >/dev/null
 
+cp -r ~/.dotfiles/zellij /workspace/data/.config/zellij
 # goto home
 cd $HOME
 
